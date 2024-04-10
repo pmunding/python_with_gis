f159afe67838e22836b79d926e201b4cae000f68
pmunding <102729357+pmunding@users.noreply.github.com>
2024-04-10 11:05:06 +0200

    add some def function

--- print("hello World").py
+++ print("hello World").py
@@ -1,1 +1,12 @@
 print("hello World")
+
+# Funktion zur Begrüßung definieren
+def begruesse_nutzer():
+    # Benutzer nach seinem Namen fragen
+    name = input("Gib deinen Namen ein: ")
+    
+    # Persönliche Begrüßung ausgeben
+    print("Hallo, " + name + "! Willkommen zur Programmierung mit Python.")
+
+# Funktion aufrufen, um den Benutzer zu begrüßen
+begruesse_nutzer()
